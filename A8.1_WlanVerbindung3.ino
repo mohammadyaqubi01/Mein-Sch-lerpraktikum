a4d30862513fb3abde047d86f3d490fc52335d4b
TomSudak <tom.sudak@icloud.com>
2024-06-18 08:34:17 +0200

    Update A8.1_WlanVerbindung3.ino

--- A8.1_WlanVerbindung3.ino
+++ A8.1_WlanVerbindung3.ino
@@ -1,89 +1,23 @@
 #include <ESP8266WiFi.h>
-const char* ssid = "WlAnLAPTOP";
-const char* password = "112233445566";
- 
-int ledPin = D4;
-WiFiServer server(80);
  
 void setup() {
   Serial.begin(115200);
-  delay(10);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  Serial.println("ESP Gestartet");
+  WiFi.begin("WlAnLAPTOP", "112233445566");
  
-  // Mit Wifi verbinden
-  Serial.print("Verbinden mit:"); 
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.println("Verbindung wird hergestellt...");
+ while (WiFi.status() != WL_CONNECTED) 
+ {
     delay(500);
     Serial.print(".");
+  {
+  Serial.println();
   }
-  Serial.println("");
-  Serial.println("WiFi verbunden");
- 
-  // Start des Servers
-  server.begin();
-  Serial.println("Server gestartet");
- 
-  // Print the IP address
-  Serial.print("Diese URL zum Verbinden aufrufen: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
+  Serial.println("Verbindung erfolgreich! IP Adresse: ");
+ Serial.println(WiFi.localIP());
 }
- 
+}
 void loop() {
-  // Prüfen, ob sich ein Client verbunden hat
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
- 
-  // Warten auf Daten vom Client
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
- 
-  // Erste Zeile des Requests lesen
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
- 
-  // Match the request
- 
-  int value = LOW;
-  if (request.indexOf("/LED=OFF") != -1)  {
-    digitalWrite(ledPin, HIGH);
-    value = HIGH;
-  }
-  if (request.indexOf("/LED=ON") != -1)  {
-    digitalWrite(ledPin, LOW);
-    value = LOW;
-  }
- 
-  // Anfrage zurücksenden
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("");
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
- 
-  client.print("Die LED ist: ");
- 
-  if(value == HIGH) {
-    client.print("Aus");
-  } else {
-    client.print("An");
-  }
-  client.println("<br><br>");
-  client.println("<a href=\"/LED=ON\"\"><button>An </button></a>");
-  client.println("<a href=\"/LED=OFF\"\"><button>Aus </button></a><br />");  
-  client.println("</html>");
- 
-  delay(1);
-  Serial.println("Client trennen");
-  Serial.println("");
+
+
 }
